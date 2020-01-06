de03e089a76764290a652e68bbe55542609fcca6
allison <allison.dixon@ssfs.org>
2020-01-06 08:51:43 -0500

    finished unit 10 option 3

--- target.py
+++ target.py
@@ -1,9 +1,8 @@
 # created by Allison Dixon
-# last updated December 13, 2019
+# last updated January 6, 2020
 # this class creates a target and assigns its point values based on colors
 
-import pygame, sys
-from pygame.locals import *
+import pygame
 
 
 class Target:
@@ -13,6 +12,10 @@ class Target:
         self.score = 0
 
     def draw_target(self):
+        """
+        This function draws a target with rings of yellow, red, blue, black, and white
+        :return: none
+        """
         x = int(self.main_surface.get_width()/2)
         y = int(self.main_surface.get_height()/2)
         pygame.draw.circle(self.main_surface, (255, 255, 254), (x, y), 200, 0)
@@ -23,10 +26,16 @@ class Target:
         pygame.draw.circle(self.main_surface, (255, 255, 0), (x, y), 40, 0)
 
     def get_score(self, position):
+        """
+        This function assigns a point value based on what color ring of the target the user clicked on
+        and calls the update_score function
+        :param position: The coordinates of the mouse
+        :return: none
+        """
         target_color = self.main_surface.get_at(position)
         if target_color == (255, 255, 0, 255):
             self.update_score(9)
-        elif target_color == (225, 0, 0, 255):
+        elif target_color == (255, 0, 0, 255):
             self.update_score(7)
         elif target_color == (0, 0, 255, 255):
             self.update_score(5)
@@ -36,13 +45,14 @@ class Target:
             self.update_score(1)
 
     def update_score(self, score):
-        self.score += score
-        self.main_surface.blit(score)
-
-    def print_score(self):
+        """
+        This function updates the user's score on the screen based on how many points they got from where they clicked
+        :param score: The user's score based on the color ring they clicked on
+        :return: none
+        """
         self.main_surface.fill((255, 255, 255))
-        self.update_score()
+        self.draw_target()
+        self.score += score
         mouse_font = pygame.font.SysFont("Helvetica", 32)
-        mouse_label = mouse_font.render(int(self.score), 1, (0, 0, 0))
+        mouse_label = mouse_font.render(str(self.score), 1, (0, 0, 0))
         self.main_surface.blit(mouse_label, (30, 30))
-
--- unit10_option3.py
+++ unit10_option3.py
@@ -1,5 +1,5 @@
 # created by Allison Dixon
-# last updated December 17, 2019
+# last updated January 6, 2020
 # unit 10 option 3: this program creates a target that lets the user click on the
 # screen to get points based on how close to the center they are, and prints their points
 
@@ -13,14 +13,15 @@ pygame.display.set_caption("Target Game")
 main_surface.fill((255, 255, 255))
 my_target = target.Target(main_surface)
 my_target.draw_target()
-my_target.update_score()
 
-
+num_clicks = 0
 while True:
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == MOUSEBUTTONDOWN:
+        # if the user clicks and has clicked less than five times, the computer will get the mouse coordinates
+        if event.type == MOUSEBUTTONDOWN and num_clicks < 5:
             my_target.get_score(pygame.mouse.get_pos())
+            num_clicks += 1
